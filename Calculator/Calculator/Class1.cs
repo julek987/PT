1cacfbce1c155bd3e74a84cd62c6fed3c9d7e70a
magda <magdalena.pakula001@gmail.com>
2022-03-12 20:37:22 +0100

    Calculator options ready, but always edefault option appears - to fix it + do unit tests

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,53 @@
+﻿namespace Calculator
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("Calculator Program");
+                Console.WriteLine("------------------");
+
+                Console.Write("Enter number 1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter option: ");
+                Console.WriteLine("a) + : Add");
+                Console.WriteLine("s) - : Substract");
+                Console.WriteLine("m) * : Multiply");
+                Console.WriteLine("d) / : Devide");
+                Console.Write("Enter a letter of option: ");
+                Console.ReadKey();
+
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        Console.WriteLine($"Result: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "s":
+                        Console.WriteLine($"Result: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "m":
+                        Console.WriteLine($"Result: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "d":
+                        Console.WriteLine($"Result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                    default:
+                        Console.WriteLine("That is not a valid option");
+                        break;
+                }
+                Console.Write("Would you like to continue? (Y = yes, N = No): ");
+
+            } while (Console.ReadLine().ToUpper() == "Y");
+        }
+    }
+}
