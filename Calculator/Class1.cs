4735e7427177605fded505aa4ff35c1f244c0700
Magdalena Pakuła 234766 <magdalena.pakula001@gmail.com>
2022-03-20 20:19:33 +0100

    New Calculator and Test with my partner :))

---
+++ Calculator/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Class1
+    {
+        double num1 = 0;
+        double num2 = 0;
+        public double Add(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+        public double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Mul(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ CalculatorTests/Class1Tests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        
+        [TestMethod()]
+        public void AddTest()
+        {
+            var calculator = new Class1();
+            double x = 5;
+            double y = 10;
+            Assert.AreEqual(15, calculator.Add(x, y));
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            var calculator = new Class1();
+            double x = 5;
+            double y = 10;
+            Assert.AreEqual(-5, calculator.Sub(x, y));
+        }
+
+        [TestMethod()]
+        public void MulTest()
+        {
+            var calculator = new Class1();
+            double x = 5;
+            double y = 10;
+            Assert.AreEqual(50, calculator.Mul(x, y));
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            var calculator = new Class1();
+            double x = 10;
+            double y = 5;
+            Assert.AreEqual(2, calculator.Div(x, y));
+        }
+    }
+}
