774e7141e164280075b53957d5f2c8202e2b8751
julek987 <76966911+julek987@users.noreply.github.com>
2022-03-20 21:18:46 +0100

    Task 0 Final

---
+++ Task 0/Calculator/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            if (y != 0) { 
+                return x / y;
+            }
+            else{
+                throw new ArgumentException("You cannot divide by zero!");
+            }
+            
+        }
+
+    }
+}
