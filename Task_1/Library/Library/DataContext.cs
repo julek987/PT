ddcdfd3374628c0ea52689522670726faac6a139
Magdalena Pakuła 234766 <magdalena.pakula001@gmail.com>
2022-04-10 21:25:43 +0200

    smol edits

--- Task_1/Library/Library/DataContext.cs
+++ Task_1/Library/Library/DataContext.cs
@@ -1,12 +1,10 @@
 ﻿namespace Library
 {
-    //TO FINISH
-    //ADD event collection
     public class DataContext
     {
         public List<Users> users = new List<Users> ();
-        public Dictionary<int, Catalog> catalogs = new Dictionary<int, Catalog>(); //idk czemu dictionary?
+        public Dictionary<int, Catalog> catalogs = new Dictionary<int, Catalog>(); 
         public List<State> states = new List<State> ();
-        public List<Event> events = new List<Event>(); //list instead of observable collection
+        public List<Event> events = new List<Event>(); 
     }
 }
--- Task_1/Library/Library/DataService.cs
+++ Task_1/Library/Library/DataService.cs
@@ -9,5 +9,14 @@
             this.dataRepository = dataRepository;
         }
 
+        public void Display(IEnumerable<Users> users)
+        {
+            List<Users> usersList = users.ToList<Users>();
+            for(int i=0; i<users.Count(); i++)
+            {
+                Console.WriteLine(usersList[i].All);
+            }
+        }
+
     }
 }
