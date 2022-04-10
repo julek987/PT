7afbd327b576efa3b0a99d3f45ed24ac04a8a98b
Magdalena Pakuła 234766 <magdalena.pakula001@gmail.com>
2022-04-10 19:42:11 +0200

    added const values and finished data contex

--- Task_1/Library/Library/DataContext.cs
+++ Task_1/Library/Library/DataContext.cs
@@ -7,5 +7,6 @@
         public List<Users> users = new List<Users> ();
         public Dictionary<int, Catalog> catalogs = new Dictionary<int, Catalog>(); //idk czemu dictionary?
         public List<State> states = new List<State> ();
+        public List<Event> events = new List<Event>(); //list instead of observable collection
     }
 }
