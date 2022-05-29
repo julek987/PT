927465a14e05740dd47544df0b9e2b8684348341
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2022-05-29 13:17:46 +0200

    update presentation

---
+++ Task_2/Service/Implementation/Service.cs
@@ -0,0 +1,16 @@
+﻿using Service.API;
+using Data.API;
+
+namespace Service.Implementation
+{
+    public class Service : IService
+    {
+        private IDataRepository dataRepository;
+
+        internal  Service(IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
+
+    }
+}
