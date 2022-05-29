fdce5460411353aa02ffc8507899b95b22b4c031
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2022-05-29 19:22:50 +0200

    all

--- Task_2/Presentation/View/MainWindow.xaml.cs
+++ Task_2/Presentation/View/MainWindow.xaml.cs
@@ -23,12 +23,102 @@ namespace Presentation
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Write_InfoID(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        private void Add_Book_Info_Button(object sender, RoutedEventArgs e)
+        private void Write_Title(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Write_Author(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Add_BookInfo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Delete_BookInfo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Book_Book_ID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Book_Info_ID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Add_Book(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Delete_Book(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Users_UserID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Users_Name(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Users_Surname(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Users_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Users_Delete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Rent_BookID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Rent_UserID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Rent_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Return_BookID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Return_UserID(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Return_Add(object sender, RoutedEventArgs e)
         {
 
         }
